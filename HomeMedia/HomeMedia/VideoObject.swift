9b8d269bb4b68f9cd00bc81dd89aea7e9846a957
Alex White <alex_p_white@yahoo.co.uk>
2015-05-25 20:14:31 +0100

    first commit of new OSX Dev area, first app is going to be a replica of the java video player.

---
+++ HomeMedia/HomeMedia/VideoObject.swift
@@ -0,0 +1,97 @@
+//
+//  ViewModel.swift
+//  HomeMedia
+//
+//  Created by Alex on 25/05/2015.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+import Foundation
+
+class Video
+{
+
+    var id: Int
+    {
+        get
+        {
+            return id
+        }
+        set
+        {
+            id = newValue
+        }
+    }
+    
+    var title: String
+    {
+        get
+        {
+            return title
+        }
+        set
+        {
+            title = newValue
+        }
+    }
+    
+    var location: String
+    {
+        get
+        {
+            return location
+        }
+        set
+        {
+            location = newValue
+        }
+    }
+    
+    var genre: String
+    {
+        get
+        {
+            return genre
+        }
+        set
+        {
+            genre = newValue
+        }
+    }
+    
+    var group: String
+    {
+        get
+        {
+            return group
+        }
+        set
+        {
+            group = newValue
+        }
+    }
+    
+    var episode: Int
+    {
+        get
+        {
+            return episode
+        }
+        set
+        {
+            episode = newValue
+        }
+    }
+    
+    var season: Int
+    {
+        get
+        {
+            return season
+        }
+        set
+        {
+            season = newValue
+        }
+    }
+}
---
+++ HomeMedia/HomeMedia/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  HomeMedia
+//
+//  Created by Alex on 25/05/2015.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func typeToggle(sender: AnyObject) {
+    }
+    
+    @IBOutlet var videoTable: [UITableView]!
+    
+    @IBAction func playBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func refreshBtn(sender: AnyObject) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
