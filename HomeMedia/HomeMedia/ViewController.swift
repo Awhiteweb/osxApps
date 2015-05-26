4b15b7a9a6cfa1fac637c3db3635a27a5f4699f8
Alex White <alex.whie@oval.uk.com>
2015-05-26 17:13:57 +0100

    updates to controller to add rows to table

--- HomeMedia/HomeMedia/ViewController.swift
+++ HomeMedia/HomeMedia/ViewController.swift
@@ -8,14 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+		let cellDetails = [ ( "title 1", "Name 1" ), ( "title 2", "Name 2" ), ( "title 3", "Name 3" ) ]
     
     @IBAction func typeToggle(sender: AnyObject) {
     }
     
-    @IBOutlet var videoTable: [UITableView]!
+	 // UI Table
+	 // data source: how many rows
+	 // delegate: what happens when rows is touched
     
+	 func numberOfSectionsInTableView(tableView: UITableView) -> Int
+	 {
+		return 1
+	 }
+	 
+	 func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	 {
+		return cellDetails.count
+	 }
+	 
+	 // needs a protoype cell in the table with identifier named cell
+	 func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+	 {
+			var cell = tableView.dequeueReusableCellWithIdentifier( "cell", forIndexPath: indexPath ) as UITableViewCell
+			
+			let ( title, author ) = cellDetails[ indexPath.row ]
+			
+			cell.textLabel?.text = title
+			
+			return cell
+	 }
+	 
+	 func tableView( tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+	 {
+		 return "Titles"
+	 }
+	 
     @IBAction func playBtn(sender: AnyObject) {
     }
     
